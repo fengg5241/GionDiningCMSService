6c1887ae19b86c4f041f15ab2e0a5526259acb82
fengg5241 <fengg5241@users.noreply.github.com>
2019-05-07 13:31:34 +0800

    Update SMSController.java

--- src/main/java/com/gion/cms/controller/SMSController.java
+++ src/main/java/com/gion/cms/controller/SMSController.java
@@ -9,23 +9,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import com.gion.cms.entity.TUser;
-import com.gion.cms.entity.TUserTransaction;
-import com.gion.cms.service.UserService;
+import com.gion.cms.entity.SMSObject;
 
 @RestController
 @RequestMapping("/sms")
 public class UserController {
 
 	
-	@PostMapping("/insert")
-	public void insert(@RequestBody TUser record) {
-		userService.insert(record);
-	}
-	
-	
-	
-	
-	
+	@PostMapping("/send")
+	public void send(@RequestBody SMSObject sms) {
+		try {
+
+		URL url = new URL("http://localhost:8080/RESTfulExample/json/product/post");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+		String input = "{\"qty\":100,\"name\":\"iPad 4\"}";
+
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+
+		if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+			throw new RuntimeException("Failed : HTTP error code : "
+				+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	 }
+	}	
 }
