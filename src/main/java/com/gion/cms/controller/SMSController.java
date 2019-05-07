aaa7d0d0867630cad100fd3b10c2c1fb2e0568e9
fengg5241 <fengg5241@users.noreply.github.com>
2019-05-07 12:54:05 +0800

    Update SMSController.java

--- src/main/java/com/gion/cms/controller/SMSController.java
+++ src/main/java/com/gion/cms/controller/SMSController.java
@@ -1,1 +1,31 @@
+package com.gion.cms.controller;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gion.cms.entity.TUser;
+import com.gion.cms.entity.TUserTransaction;
+import com.gion.cms.service.UserService;
+
+@RestController
+@RequestMapping("/sms")
+public class UserController {
+
+	
+	@PostMapping("/insert")
+	public void insert(@RequestBody TUser record) {
+		userService.insert(record);
+	}
+	
+	
+	
+	
+	
+}
