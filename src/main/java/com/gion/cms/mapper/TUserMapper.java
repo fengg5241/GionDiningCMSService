e1a75fb6e559856eb5b3a98bb0245289b96ac16f
fengg5241 <fengg5241@users.noreply.github.com>
2019-07-03 16:43:57 +0800

    Update TUserMapper.java

--- src/main/java/com/gion/cms/mapper/TUserMapper.java
+++ src/main/java/com/gion/cms/mapper/TUserMapper.java
@@ -56,6 +56,8 @@ public interface TUserMapper {
     
     int updatePassword(TUser user);
     
+    int updatePath(TUser user);
+    
     int addPoints(TUser user);
     
     TUser selectByPhone(String phone);
